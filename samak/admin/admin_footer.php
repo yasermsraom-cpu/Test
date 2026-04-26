ff43cd858d1a45c88d167888e0188c855058feba
yasermsraom-cpu <yaser.msra.om@gmail.com>
2026-04-26 10:58:24 +0400

    Delete samak directory

--- samak/admin/admin_footer.php
+++
@@ -1,7 +0,0 @@
-        </section>
-    </div>
-</div>
-
-<script src="../assets/js/main.js"></script>
-</body>
-</html>
--- samak/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin_id']);
-unset($_SESSION['admin_user']);
-header("Location: login.php");
-exit;
-?>
